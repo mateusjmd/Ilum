9c9e3402d1ee903115fced6fe891f27c7ee1ea99
Mateus de Jesus Mendes <mateus25032@ilum.cnpem.br>
2026-03-29 17:42:07 -0300

    Template de SLURM para script Python

---
+++ 2026.1/HPC/job_py.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=nome_job
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16G
+#SBATCH --nodelist=work1
+#SBATCH --time=DD-HH:MM:SS
+#SBATCH --output=logs/saida_%j.out
+#SBATCH --error=logs/erro_%j.err
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=usuarioXXX@heisenberg.cnpem.br
+
+# ---- Preparacao do ambiente ----
+mkdir -p logs
+source ~/.bashrc
+conda activate meu_env
+
+# ---- Execucao ----
+python script.py --argumentos
